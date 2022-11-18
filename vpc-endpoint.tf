257ae11fc3fb328786c2010b498ab460d1069648
Mulin, Andre <andre.mulin@zoi.tech>
2022-11-18 16:06:20 +0000

    fixed data region

--- vpc-endpoint.tf
+++ vpc-endpoint.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_endpoint" "endpoint" {
   count               = var.vpcs[0].id == "" ? 0 : length(var.vpcs)
-  service_name        = "com.amazonaws.${aws_region.current.name}.ssm"
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssm"
   vpc_id              = var.vpcs[count.index].id
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
