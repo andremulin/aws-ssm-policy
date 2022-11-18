1a7f36824638a8acaa2a4d97cae80a5fa2e13076
Mulin, Andre <andre.mulin@zoi.tech>
2022-11-18 16:04:16 +0000

    fixed variables

--- vpc-endpoint.tf
+++ vpc-endpoint.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_endpoint" "endpoint" {
-  count               = var.vpcs == "" ? 0 : length(var.vpcs)
+  count               = var.vpcs[0].id == "" ? 0 : length(var.vpcs)
   service_name        = "com.amazonaws.${aws_region.current.name}.ssm"
   vpc_id              = var.vpcs[count.index].id
   vpc_endpoint_type   = "Interface"
